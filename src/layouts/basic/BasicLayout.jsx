9437dac6c0d69dcb99cdcd526260550447b78755
Gabriel Ballester <gaby.ballester@gmail.com>
2021-04-26 15:51:36 +0200

    Setup BasicLayout + routes

---
+++ src/layouts/basic/BasicLayout.jsx
@@ -0,0 +1,30 @@
+import { Route } from "react-router-dom";
+// Components
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer";
+// styles
+import "./BasicLayout.scss";
+
+export default function BasicLayout(props) {
+  const { routes } = props;
+  return (
+    <div>
+      <Header />
+      <div className="content">
+        <LoadRoutes routes={routes} />
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+function LoadRoutes({ routes }) {
+  return routes.map((route, index) => (
+    <Route
+      key={index}
+      path={route.path}
+      exact={route.exact}
+      component={route.component}
+    />
+  ));
+}
