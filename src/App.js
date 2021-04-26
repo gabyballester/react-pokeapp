126f4f524e66fad96151e7dee703f1d35a5fdbd0
Gabriel Ballester <gaby.ballester@gmail.com>
2021-04-26 13:09:55 +0200

    Install & setup router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import './App.scss';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="app">
-      <h1>Pokeapp <span>con react</span></h1>
-    </div>
+    <Router>
+      <div className="app">
+        <h1>Pokeapp <span>con react</span></h1>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route component={Error404} />
+        </Switch>
+      </div>
+    </Router>
   );
+}
+
+function Home() {
+  return <h2>Vista Home</h2>
+}
+
+function Login() {
+  return <h2>Vista Login</h2>
+}
+
+function Error404() {
+  return <h2>Error 404</h2>
 }
 
 export default App;
